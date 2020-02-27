9431452e0527ee7392fdf6235047e03bc2742cd9
EntiViod <56116348+EntiViod@users.noreply.github.com>
2020-02-27 11:13:07 +0100

    Push Boids

---
+++ Boids_Enti/Assets/Agent.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour
+{
+    public Vector3 velocity;
+
+    public Vector3 separationForce, cohesionForce, alignmentForce;
+    [SerializeField]
+    float radius = 1.0f;
+
+    public List<Agent> neightbours;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + separationForce);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(transform.position, transform.position + cohesionForce);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, transform.position + alignmentForce);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + velocity);
+
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, radius);
+
+      
+    }
+
+    private void Awake()
+    {
+        neightbours = new List<Agent>();
+    }
+    
+    public enum DEBUGforceType
+    {
+        SEPARATION,
+        COHESION,
+        ALIGNMENT
+    }
+    public void addForce(Vector3 f, DEBUGforceType type)
+    {
+        if (type == DEBUGforceType.SEPARATION)
+            separationForce = f;
+        else if (type == DEBUGforceType.COHESION)
+            cohesionForce = f;
+        else
+            alignmentForce = f;
+
+        velocity += f;
+    }
+    public void addForce(Vector3 f)
+    {
+        velocity += f;
+    }
+    public void updateAgent()
+    {
+        transform.position += velocity * Time.deltaTime;
+    }
+    public void checkNeightbours()
+    {
+        Collider[] checks = Physics.OverlapSphere(transform.position, radius);
+        foreach (Collider c in checks)
+        {
+            neightbours.Add(c.GetComponent<Agent>());
+        }
+       
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Boids_Enti/Assets/Boids.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boids : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject agentPrefab;
+
+    [SerializeField]
+    int numBoids = 10;
+
+    Agent[] agents;
+
+    [SerializeField]
+    float agentRadius = 2.0f;
+
+    [SerializeField]
+    float separationWeight = 1.0f, cohesionWeight = 1.0f, alignmentWeight = 1.0f;
+
+    private void Awake()
+    {
+        List<Agent> agentlist = new List<Agent>();
+
+        for(int i = 0; i<numBoids; i++)
+        {
+            Vector3 position = Vector3.up * Random.Range(0, 10)
+                + Vector3.right * Random.Range(0, 10) + Vector3.forward * Random.Range(0, 10);
+            agentlist.Add(Instantiate(agentPrefab, position, Quaternion.identity).GetComponent<Agent>());
+
+        }
+        agents = agentlist.ToArray();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Agent a in agents)
+        {
+            a.velocity = Vector3.zero;
+            checkForNeightBours(a);
+            calculateSeparation(a);
+            calculateAlignment(a);
+            calculateCohesion(a);
+            a.updateAgent();
+            a.neightbours.Clear();
+         
+        }
+    }
+
+    void checkForNeightBours(Agent a)
+    {
+        
+    }
+
+    void calculateSeparation(Agent a)
+    {
+        a.addForce(Vector3.up, Agent.DEBUGforceType.SEPARATION);
+       
+    }
+
+    void calculateCohesion(Agent a)
+    {
+        a.addForce(Vector3.forward, Agent.DEBUGforceType.COHESION);
+
+    }
+
+    void calculateAlignment(Agent a)
+    {
+        a.addForce(Vector3.right, Agent.DEBUGforceType.ALIGNMENT);
+    }
+}
